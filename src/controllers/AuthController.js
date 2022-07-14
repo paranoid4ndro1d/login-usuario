3ace9882e4fb523837d23f5205b31bf1482c1f3e
paranoid4ndro1d <marcio.morais96@outlook.com>
2022-07-14 20:04:24 -0300

    atualização aula login-usuario

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,22 +1,59 @@
+const fs = require("fs");
+const path = require("path");
+
 const authController = {
-    // Tela para cadastro do usuário
-    register: (req, res) => {
+  // Tela para cadastro do usuário
+  register: (req, res) => {
+    return res.render("register", {
+      title: "Cadastro",
+    });
+  },
+  // Processamento do cadastro do usuário
+  create: (req, res) => {
+    const usersJson = fs.readFileSync(
+      // Caminho do arquivo
+      path.join(__dirname, "..", "data", "users.json"),
+      // Formato de leitura
+      "utf-8"
+    );
+    const users = JSON.parse(usersJson);
+
+    const { nome, sobrenome, apelido, email, senha, confirmar_senha } =
+      req.body;
+    if (
+      !nome ||
+      !sobrenome ||
+      !apelido ||
+      !email ||
+      !senha ||
+      !confirmar_senha
+    ) {
       return res.render("register", {
         title: "Cadastro",
+        error: {
+          message: "Preencha todos os campos",
+        },
       });
-    },
-    // Processamento do cadastro do usuário
-    create: (req, res) => {},
-    // Tela para realizar login
-    login: (req, res) => {
-      return res.render("login", {
-        title: "Login",
+    }
+    if(senha !== confirmar_senha){
+      return res.render("register", {
+        title: "Cadastro",
+        error: {
+          message: "Senhas não coincidem",
+        },
       });
-    },
-    // Processamento do login
-    auth: (req, res) => {},
-    // Processamento do deslogar
-    logout: (req, res) => {},
-  };
-  
-  module.exports = authController;
+    }
+  },
+  // Tela para realizar login
+  login: (req, res) => {
+    return res.render("login", {
+      title: "Login",
+    });
+  },
+  // Processamento do login
+  auth: (req, res) => {},
+  // Processamento do deslogar
+  logout: (req, res) => {},
+};
+
+module.exports = authController;
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/AuthController");
 
 router.get("/login", authController.login);
 router.get("/register", authController.register);
+router.post("/register", authController.create)
 
 module.exports = router;
